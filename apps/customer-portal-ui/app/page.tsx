65a03c456148c16858f274cf4b1a88b449010d09
Arnis Zeps <zeps.arnis@gmail.com>
2025-02-28 00:38:00 +0200

    feat: page structure improvement

--- apps/customer-portal-ui/app/page.tsx
+++ apps/customer-portal-ui/app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex text-white flex-col gap-6 md:gap-48 h-full bg-gradient-to-r from-violet-700 to-sky-600 justify-center ">
-      <div className="w-full px-0 flex flex-col items-center max-w-7xl mx-auto md:gap-16 md:flex-row md:px-16">
-        <div className="flex py-10 flex-col gap-6 items-center md:items-start md:gap-12">
+    <div className="h-full">
+      <div className="text-white w-full h-full px-0 flex flex-col items-center justify-center md:gap-16 md:flex-row md:px-16  bg-gradient-to-r from-violet-700 to-sky-600 ">
+        <div className="flex flex-col gap-6 items-center md:items-start md:gap-12 ">
           <h1 className="text-2xl">Professional resume builder</h1>
           <div className="max-w-96 font-medium border-l-4 border-violet-900 pl-4 space-y-2 md:max-w-full">
             <p>Build best looking resume in minutes!</p>
@@ -19,9 +19,10 @@ export default function Home() {
           </div>
         </div>
         <div className="max-w-xl">
-          <Image className="w-[100%] md:w-[450px] h-auto rounded-3xl" src={"/hero-1.jpg"} alt="hero-1" width={450} height={0} />
+          <Image className="w-[100%] md:w-[440px] h-auto" src={"/hero-1.jpg"} alt="hero-1" width={450} height={0} />
         </div>
       </div>
-   </div>
+    </div>
+
   );
 }
