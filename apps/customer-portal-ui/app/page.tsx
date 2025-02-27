702b2c6a3badd665971fa11654b8bf3d5817ddb1
Arnis Zeps <zeps.arnis@gmail.com>
2025-02-28 00:00:21 +0200

    style: improved overall styling

--- apps/customer-portal-ui/app/page.tsx
+++ apps/customer-portal-ui/app/page.tsx
@@ -3,27 +3,25 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-6 md:gap-48 h-full just">
-      <div className="bg-stone-200">
-        <div className="px-0 flex flex-col items-center max-w-7xl mx-auto md:gap-16 md:flex-row md:place-items-center md:px-16">
-          <div className="flex py-10 flex-col gap-6 items-center md:items-start md:gap-12">
-            <h1 className="text-2xl">Professional resume builder</h1>
-            <div className="max-w-96 font-medium border-l-4 border-black pl-4 space-y-2 md:max-w-full">
-              <p>Build best looking resume in minutes!</p>
-              <p>Only 2% of resumes win. Yours will be one of them. Lets build you a resume that works.</p>
-              <p>Use best templates</p>
-              <p>Cheap price - 4.99$ monthly</p>
-              <p>Change your job search game with Resumex!</p>
-            </div>
-            <div className="w-full flex justify-center">
-              <LinkButton text={"Create now!"} href="http://www.google.com" />
-            </div>
+    <div className="flex text-white flex-col gap-6 md:gap-48 h-full bg-gradient-to-r from-violet-700 to-sky-600 justify-center ">
+      <div className="w-full px-0 flex flex-col items-center max-w-7xl mx-auto md:gap-16 md:flex-row md:px-16">
+        <div className="flex py-10 flex-col gap-6 items-center md:items-start md:gap-12">
+          <h1 className="text-2xl">Professional resume builder</h1>
+          <div className="max-w-96 font-medium border-l-4 border-violet-900 pl-4 space-y-2 md:max-w-full">
+            <p>Build best looking resume in minutes!</p>
+            <p>Only 2% of resumes win. Yours will be one of them. Lets build you a resume that works.</p>
+            <p>Use best industry templates</p>
+            <p>2.99$ monthly - no hidden costs</p>
+            <p>Change your job search game with Resumex!</p>
           </div>
-          <div className="max-w-xl">
-            <Image className="w-[100%] md:w-[450px] h-auto" src={"/hero-1.jpg"} alt="hero-1" width={450} height={0} />
+          <div className="w-full flex justify-center">
+            <LinkButton text={"Create now!"} href="/dashboard" />
           </div>
         </div>
+        <div className="max-w-xl">
+          <Image className="w-[100%] md:w-[450px] h-auto rounded-3xl" src={"/hero-1.jpg"} alt="hero-1" width={450} height={0} />
+        </div>
       </div>
-    </div>
+   </div>
   );
 }
--- apps/customer-portal-ui/components/LinkButton/index.tsx
+++ apps/customer-portal-ui/components/LinkButton/index.tsx
@@ -3,7 +3,7 @@ import ILinkButtonProps from "@/interfaces/ILinkButtonProps";
 export default function LinkButton({ text, href }: ILinkButtonProps) {
   return (
     <a href={href}>
-      <button className="bg-white text-md hover:bg-stone-400 text-black flex items-center gap-2 px-10 py-5 rounded-3xl font-medium">{text}</button>
+      <button className="bg-white text-md hover:bg-violet-200 text-black flex items-center gap-2 px-10 py-5 rounded-3xl font-medium">{text}</button>
     </a>
   );
 }
